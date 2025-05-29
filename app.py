90b60fa5353e4a697571ec3cccec4f40ab8989c2
ILHOM <ilhomtuhtamurodov88@gmail.com>
2025-05-29 13:10:51 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+#1
+print("ILHOMBEK")
+#2
+print(1+2)
+#3
+print("game",364,"-"*20,"\"",3.4,-1, sep="$")
+#4
+print("I",end="")
+print("am")
+#5
+print("-"*20)
+print("{","I","L","H","O","M","}",sep="}~{",end="\n"+"_"*20)
